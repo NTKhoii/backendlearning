64b3b6cdcea4b5c566e07a3a67c7103fdd5ed661
NTKhoii <trongkhoi2432005@gmail.com>
2023-11-05 16:06:44 +0700

    init project

--- src/server.js
+++ src/server.js
@@ -1,29 +1,22 @@
-
+require('dotenv').config()
 const express = require('express')
 const path = require('path')
-require('dotenv').config()
 const app = express()//app express
 const port = process.env.PORT || 8888 //port
 const hostname = process.env.HOST_NAME
-
+const configViewengine = require('./config/viewengine')
+const Webroutes = require('./routes/web')
 // console.log(">>> check env:", process.env)
 // console.log('process.env.PORT')
 
 //config template engine
-app.set('views', path.join(__dirname, 'views'))
-app.set('view engine', 'ejs')
+configViewengine(app);
 
 //config static file
-app.use(express.static(path.join(__dirname, 'public')))
+
 
 //khai báo route
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.get('/abc', (req, res) => {
-    // res.send('Hello Viet Nam')
-    res.render('sample.ejs')
-})
+app.use('/', Webroutes)
 app.listen(port, hostname, () => {
     console.log(`Example app listening on port ${port}`)
 })
